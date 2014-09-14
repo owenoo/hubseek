9058db26324b63a8dca21045b10519d6f9dc7f9f
Asmodeus <devil.unity@gmail.com>
2014-09-14 16:54:48 -0500

    Added users resource.  Added login screen.  Ran migration to add fields to user table.

--- db/migrate/20140914210636_users_update_fields.rb
+++ db/migrate/20140914210636_users_update_fields.rb
@@ -1,10 +1,8 @@
 class UsersUpdateFields < ActiveRecord::Migration
   def change
-  	create_table :users do |t|
-      t.string :username 
-      t.string :email
-      t.string :encrypted_password 
-      t.string :salt
-      t.timestamps
+  	add_column :users, :username, :string
+  	add_column :users, :email, :string
+  	add_column :users, :encrypted_password, :string
+  	add_column :users, :salt, :string
   end
 end

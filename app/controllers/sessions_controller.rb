f844db1ea0aad6bce99b96b8fed2e36d43fa83fd
Asmodeus <devil.unity@gmail.com>
2014-09-18 02:14:18 -0500

    testing create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,8 @@
 class SessionsController < ApplicationController
 
 	#controller callbacks
-	before_filter :authenticate_user, :only => [:home, :profile, :setting]
-	before_filter :save_login_state, :only => [:login, :login_attempt]
+	#before_filter :authenticate_user, :only => [:home, :profile, :setting]
+	#before_filter :save_login_state, :only => [:login, :login_attempt]
 
 	#methods
 	def login
@@ -14,17 +14,17 @@ class SessionsController < ApplicationController
 		redirect_to :action => 'login'
 	end
 
-	def login_attempt
-		authorized_user = User.authenticate(params[:username_or_email],params[:login_password])
-		if authorized_user			
-			session[:user_id] = authorized_user.id
-			flash[:notice] = "Hello again, #{authorized_user.username}"
-			redirect_to(:action => 'home')
-		else
-			flash[:notice] = "Invalid Username or Password"
-			flash[:color]= "invalid"
-			render "login"	
-		end
+	def login_attempt		
+		#authorized_user = User.authenticate(params[:username_or_email],params[:login_password])
+		#if authorized_user			
+		#	session[:user_id] = authorized_user.id
+		#	flash[:notice] = "Hello again, #{authorized_user.username}"
+		#	redirect_to(:action => 'home')
+		#else
+		#	flash[:notice] = "Invalid Username or Password"
+		#	flash[:color]= "invalid"
+		#	render "login"	
+		#end
 	end
 
 	def home
@@ -48,6 +48,9 @@ class SessionsController < ApplicationController
 	end
 
 	def create
+		@user = User.find_or_create_for_github(env["omniauth.auth"])
+		flash[:notice] = "Signed in with GitHub successfully."
+		sign_in_and_redirect @user, :event => :authentication
 	end
 
 	def failure

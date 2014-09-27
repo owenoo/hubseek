8b634470ba9259a8a826424fda61d4f3a4834c60
Asmodeus <devil.unity@gmail.com>
2014-09-27 18:05:48 -0500

    Fleshed out webpage; added signout link

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,31 +1,6 @@
-class UsersController < ApplicationController
-
-	#controller callbacks
-	#before_filter :save_login_state, :only => [:new, :create]
-
-	#methods
-	def user_params
-		params.require(:user).permit(:username, :email, :password, :password_confirmation)
-	end	
+class UsersController < ApplicationController	
 
 	def new
-		@user = User.new
 	end
-	
-	def create
-		
-		@user = User.new(user_params)
-		
-		if @user.save
-			flash[:notice] = "You signed up successfully"
-			flash[:color]= "valid"
-		else
-			flash[:notice] = "Form is invalid"
-			flash[:color]= "invalid"
-		end
-		render "new"
-	end
-
-
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,25 +1,5 @@
 class User < ActiveRecord::Base		
 
-	def init
-	end
-
-	def self.find_or_create_for_github(response)
-		data = response['extra']['user_hash']
-		if user = User.find_by_github_id(data["id"])         # Find user
-			user
-		else                                         
-			# Create a user with
-			# a stub password.
-			user = User.new(:email => "github+#{data["id"]}@example.com",
-			:password => Devise.friendly_token[0,20])
-			user.github_id = data["id"]
-			user.github_screen_name = data["screen_name"]
-			user.github_display_name = data["display_name"]
-			user.confirm!
-			user
-		end
-	end
-
 	def self.create_with_omniauth(auth)
 	    create! do |user|
 			user.provider = auth["provider"]

17b1bc1786a8eb76d04ee05cdc4bc2549eabcb7b
Asmodeus <devil.unity@gmail.com>
2014-09-27 17:29:51 -0500

    code on wrong lines; fixed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -69,10 +69,9 @@ class SessionsController < ApplicationController
 	#Natasha the robot
 	def create     
 		auth = request.env["omniauth.auth"]     
-		user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || 
-											User.create_with_omniauth(auth)     
-											session[:user_id] = user.id     
-											redirect_to root_url, :notice => "Signed in!"
+		user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)     
+		session[:user_id] = user.id     
+		redirect_to root_url, :notice => "Signed in!"
 	end
  
 	def destroy

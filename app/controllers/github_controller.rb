dff8e353dd71406712c1615a4edb4fca0cdaa590
Asmodeus <devil.unity@gmail.com>
2014-09-28 22:19:18 -0500

    method name change

--- app/controllers/github_controller.rb
+++ app/controllers/github_controller.rb
@@ -1,14 +1,15 @@
 class GithubController < ApplicationController
 
-	def search 
+	def search_github 
 
+		binding.pry
 		x = GithubClient.new
 		x.get_repo_list
 
 	end
 
 	def home
-		
+
 	end
 
 end

c4b5073ceb259c84a545f900ccb6986ee99ad169
Asmodeus <devil.unity@gmail.com>
2014-09-28 19:58:05 -0500

    rescue exception

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,9 +7,11 @@ class ApplicationController < ActionController::Base
  
 	private
 
-	def current_user
-		binding.pry
+	def current_user		
 		@current_user ||= User.find(session[:user_id]) if session[:user_id]
+
+		rescue ActiveRecord::RecordNotFound => e
+  		@current_user = nil
 	end
 	 
 end

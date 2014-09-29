58cdb34cd04d453cb09c005fab05a73c3f4cc1b2
Asmodeus <devil.unity@gmail.com>
2014-09-28 22:50:12 -0500

    added search method..  untested

--- app/controllers/github_controller.rb
+++ app/controllers/github_controller.rb
@@ -1,9 +1,10 @@
 require 'json'
+require 'rest-client'
+require 'rest_client'
 
 class GithubController < ApplicationController
 
 	def search_github 
-
 		binding.pry
 		x = GithubClient.new
 		x.get_repo_list
@@ -13,9 +14,15 @@ class GithubController < ApplicationController
 	def home
 		binding.pry
 		if request != nil
-			flash[:requestData] = JSON.parse(request.to_json)			
+			flash[:requestData] = JSON.parse(request.env.to_json)			
 		end
-
 	end
 
+	def basic_search
+
+		response = RestClient.get '/search/repositories', {:params => {:q => tetris, 'language' => 'assembly', :sort => 'star', :order => 'desc'}}
+
+		flash[:response_str] = response.to_str
+		binding.pry
+	end
 end

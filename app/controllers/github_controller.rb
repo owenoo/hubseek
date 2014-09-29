19522fa64b0b7d19d5323547155f4628cf703958
Asmodeus <devil.unity@gmail.com>
2014-09-28 23:15:48 -0500

    stack level too deep error; simplifying code

--- app/controllers/github_controller.rb
+++ app/controllers/github_controller.rb
@@ -12,15 +12,15 @@ class GithubController < ApplicationController
 	end
 
 	def home
-		binding.pry
-		if response != nil
-			flash[:responseData] = JSON.parse(response.to_json)			
-		end
+		#binding.pry
+		#if response != nil
+		#	flash[:responseData] = JSON.parse(response.to_json)			
+		#end
 	end
 
 	def basic_search
 		binding.pry
-		response = RestClient.get '/search/repositories', {:params => {:q => tetris, 'language' => 'assembly', :sort => 'star', :order => 'desc'}}
+		response = RestClient.get '/search/repositories' #, {:params => {:q => tetris, 'language' => 'assembly', :sort => 'star', :order => 'desc'}}
 
 		flash[:response_str] = response.to_str
 		binding.pry

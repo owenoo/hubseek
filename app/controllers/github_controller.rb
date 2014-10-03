0192097590b50d08adefbbf1d2ba45090e8c9744
Asmodeus <devil.unity@gmail.com>
2014-10-03 00:40:37 -0500

    fun with hashes

--- app/controllers/github_controller.rb
+++ app/controllers/github_controller.rb
@@ -20,22 +20,58 @@ class GithubController < ApplicationController
 
 	def basic_search
 		
-		response = RestClient.get 'https://api.github.com/search/repositories?q=game&page=1&per_page=100' 
+		response = RestClient.get 'https://api.github.com/search/repositories?q=game&page=1&per_page=3' 
 		#, {:params => {:q => tetris, 'language' => 'assembly', :sort => 'star', :order => 'desc'}}
 
-		flash[:responseData] = JSON.parse(response)
-		flash[:firstHit] = flash[:responseData]["items"][0]
+		hash_response = JSON.parse(response)
+		items = hash_response["items"]
+		@search_result = map_items(items)
+		#flash[:htmlGithubList] = display_all(list)
+
+		flash[:responseData] = hash_response
+		flash[:firstHit] = hash_response["items"][0]		
+
+
+
+
 		
 		redirect_to url_for(:controller => 'github', :action => 'home'), :notice => "Search complete." 
 	end
 
+	def map_items (items)
+
+		# items = [ {},{}, ... , {} ]
+		result = []
+		
+		items.each do |item_hash|
+			elem = {}
+			elem.id = item_hash["id"]
+			elem.name = item_hash["name"]
+			elem.url = item_hash["url"]
+			elem.score = item_hash["score"]
+			result.push(elem)
+		end
+
+		return result
+	end
+
 	def debug_getdata
 
-		response = RestClient.get 'https://api.github.com/search/repositories?q=game&page=1&per_page=100' 
+		response = RestClient.get 'https://api.github.com/search/repositories?q=game&page=1&per_page=3' 
 		#, {:params => {:q => tetris, 'language' => 'assembly', :sort => 'star', :order => 'desc'}}
 
 		JSON.parse(response)
 
+	end
+
+	def display_all(collection)
+		content_tag(:ul, :class => "list") do
+			collection.collect do |member|
+				content_tag(:li, :id => member.name.gsub(' ', '-').downcase.strip) do
+					member.name
+				end
+	    	end
+	  	end
 	end
 
 =begin
